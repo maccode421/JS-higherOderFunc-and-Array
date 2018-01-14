3100124da59d586fa8ab6b0547e25e38a0209b25
maccode421 <maccavalli@yahoo.com>
2018-01-14 09:45:31 -0500

    minor fix

--- main.js
+++ main.js
@@ -6,9 +6,9 @@ const companies = [
   {name: "Company Five", category: "Technology", start: 2009, end: 2014},
   {name: "Company Six", category: "Finance", start: 1987, end: 2010},
   {name: "Company Seven", category: "Auto", start: 1986, end: 1996},
-  {name: "Company Eight", category: "Technology", start: 20011, end: 2016},
+  {name: "Company Eight", category: "Technology", start: 2011, end: 2016},
   {name: "Company Nine", category: "Retail", start: 1981, end: 2003},
-  {name: "Company Ten", category: "Aguaponic", start: 1981, end: 2017}
+  {name: "Company Ten", category: "Aguaponic", start: 2012, end: 2017}
 ];
 
 const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
